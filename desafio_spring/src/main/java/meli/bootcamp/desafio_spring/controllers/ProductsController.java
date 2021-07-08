b7b6e893b2474fbcdf1bdb3f6f641d9fab8886d6
Bruno Waldvogel <bruno.waldvogel@mercadolivre.com>
2021-07-08 18:22:04 -0300

    Implementado CRUD de produtos

---
+++ desafio_spring/src/main/java/meli/bootcamp/desafio_spring/controllers/ProductsController.java
@@ -0,0 +1,60 @@
+package meli.bootcamp.desafio_spring.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import meli.bootcamp.desafio_spring.dtos.ProductDTO;
+import meli.bootcamp.desafio_spring.dtos.UpsertProductDTO;
+import meli.bootcamp.desafio_spring.services.ProductService;
+
+@RestController
+@RequestMapping("products")
+public class ProductsController {
+    
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts() {
+        return this.productService.getAllProducts();
+    }
+    
+    @GetMapping("{productId}")
+    public ProductDTO findProductById(@PathVariable Long productId) {
+        return this.productService.findProductById(productId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO addProduct(@RequestBody @Valid UpsertProductDTO upsertProductDTO) {
+        return this.productService.addProduct(upsertProductDTO);
+    }
+
+    @PutMapping("{productId}")
+    public ProductDTO updateProduct(
+            @PathVariable Long productId,
+            @RequestBody @Valid UpsertProductDTO upsertProductDTO) {
+        return this.productService.updateProduct(productId, upsertProductDTO);
+    }
+
+    @DeleteMapping("{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long productId) {
+        this.productService.deleteProduct(productId);
+    }
+}
--- desafio_spring/src/main/java/meli/bootcamp/desafio_spring/services/ProductService.java
+++ desafio_spring/src/main/java/meli/bootcamp/desafio_spring/services/ProductService.java
@@ -1,22 +1,84 @@
 package meli.bootcamp.desafio_spring.services;
 
+import meli.bootcamp.desafio_spring.dtos.UpsertProductDTO;
+import meli.bootcamp.desafio_spring.dtos.ProductDTO;
+import meli.bootcamp.desafio_spring.entities.Category;
 import meli.bootcamp.desafio_spring.entities.Product;
 import meli.bootcamp.desafio_spring.exceptions.ResourceNotFoundException;
 import meli.bootcamp.desafio_spring.repositories.ProductRepository;
+import java.util.stream.Collectors;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryService categoryService;
 
-    public ProductService(ProductRepository productRepository) {
+    
+    public ProductService(
+            ProductRepository productRepository,
+            CategoryService categoryService) {
         this.productRepository = productRepository;
+        this.categoryService = categoryService;
     }
 
     public Product getProductById(Long productId) throws ResourceNotFoundException {
         return productRepository.findById(productId).orElseThrow(() ->
                 new ResourceNotFoundException("Product " + productId + " does not exist.")
         );
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = this.productRepository.findAll();
+        return products.stream().map(ProductDTO::toDTO).collect(Collectors.toList());
+    }
+
+    public ProductDTO findProductById(Long productId) {
+        Product product = getProductById(productId);
+        return ProductDTO.toDTO(product);
+    }
+
+    public ProductDTO addProduct(UpsertProductDTO upsertProductDto) {
+        Long categoryId = upsertProductDto.getCategoryId();
+        Category category = this.categoryService.findById(categoryId);
+
+        Product product = new Product(
+            upsertProductDto.getProductName(),
+            upsertProductDto.getType(),
+            upsertProductDto.getBrand(),
+            upsertProductDto.getColor(),
+            upsertProductDto.getNotes(),
+            category
+        );
+
+        Product savedProduct = this.productRepository.save(product);
+
+        return ProductDTO.toDTO(savedProduct);
+    }
+
+    public ProductDTO updateProduct(Long productId, UpsertProductDTO upsertProductDTO) {
+        Product product = getProductById(productId);
+        Category category = this.categoryService.findById(upsertProductDTO.getCategoryId());
+
+        product.setName(upsertProductDTO.getProductName());
+        product.setType(upsertProductDTO.getType());
+        product.setBrand(upsertProductDTO.getBrand());
+        product.setColor(upsertProductDTO.getColor());
+        product.setNotes(upsertProductDTO.getNotes());
+        product.setCategory(category);
+
+        Product updatedProduct = this.productRepository.save(product);
+        
+        return ProductDTO.toDTO(updatedProduct);
+    }
+
+    public void deleteProduct(Long productId) {
+        Product product = getProductById(productId);
+        
+        this.productRepository.delete(product);
     }
 }
